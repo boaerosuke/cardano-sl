bedae4f0efee613b15ddf62efa9c5562fe884194
Artyom <yom@artyom.me>
2017-11-01 02:43:05 +0300

    [CSL-1279] Remove .GodTossing from lots of modules

--- ssc/Pos/Ssc/GodTossing/Toss/Pure.hs
+++ ssc/Pos/Ssc/GodTossing/Toss/Pure.hs
@@ -30,7 +30,7 @@ import           Pos.Ssc.GodTossing.Toss.Class  (MonadToss (..), MonadTossEnv (.
                                                  MonadTossRead (..))
 import           Pos.Ssc.Types                  (SscGlobalState, sgsCommitments, sgsOpenings,
                                                  sgsShares, sgsVssCertificates)
-import qualified Pos.Ssc.GodTossing.VssCertData as VCD
+import qualified Pos.Ssc.VssCertData            as VCD
 
 type MultiRichmenStakes = HashMap EpochIndex RichmenStakes
 type MultiRichmenSet   = HashMap EpochIndex RichmenSet
